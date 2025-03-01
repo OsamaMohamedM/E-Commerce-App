df985ec13be7071c3b1c1da6f4f269f117c572ff
osama <osamamoahmed242@gmail.com>
2025-03-02 00:32:02 +0200

    splash screen ui done

---
+++ lib/Core/utils/assetsImages.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.svg
+  static const String assetsImagesLogo = "assets/images/logo.svg";
+
+  /// Assets for assetsImagesPlant
+  /// assets/images/plant.png
+  static const String assetsImagesPlant = "assets/images/plant.png";
+
+  /// Assets for assetsImagesSplashBottom
+  /// assets/images/splashBottom.svg
+  static const String assetsImagesSplashBottom = "assets/images/splashBottom.svg";
+}
+
