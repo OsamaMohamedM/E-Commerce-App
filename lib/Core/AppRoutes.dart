a8043ca7d7cfd4449a71020431c58df355674a75
osama <osamamoahmed242@gmail.com>
2025-03-01 23:59:28 +0200

    Auto-generated commit message

--- lib/Core/AppRoutes.dart
+++ lib/Core/AppRoutes.dart
@@ -2,7 +2,7 @@ import 'package:e_commerce/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 class Approutes {
-  static const String splash = '/splash';
+  static const String splash = '/';
 
   static final GoRouter router = GoRouter(routes: [
     GoRoute(path: splash , builder: (context, state) =>const SplashView() ,)
--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/SplashViewBody.dart';
+
 class SplashView extends StatelessWidget {
 const SplashView({super.key});
 
   @override
+
   Widget build(BuildContext context){
     return const Scaffold(
-      body: Column(),
+      body: SafeArea(child: SplashViewBody()),
     );
   }
 }
