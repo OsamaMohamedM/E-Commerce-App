e976aaae86ff26e84cf6872fd44573340b2a85ca
osama <osamamoahmed242@gmail.com>
2025-03-02 03:05:00 +0200

    add Localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce/Core/AppRoutes.dart';
+import 'package:e_commerce/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +13,14 @@ const MyApp({super.key});
   @override
   Widget build(BuildContext context){
     return MaterialApp.router(
+           localizationsDelegates: const[
+                 S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+            locale: const Locale('ar'),
       debugShowCheckedModeBanner: false,
       routerConfig:Approutes.router ,
     );
