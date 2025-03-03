59a1328abeff4ea5eb2472b1b3baff10f19ce22d
osama <osamamoahmed242@gmail.com>
2025-03-03 23:08:04 +0200

    init loginView

---
+++ lib/features/Auth/Login/Presentation/views/LoginView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/LoginViewBody.dart';
+
+class LoginView extends StatelessWidget {
+const LoginView({ super.key });
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: LoginViewBody(),
+    );
+  }
+}
---
+++ lib/features/Auth/Login/Presentation/views/widgets/LoginViewBody.dart
@@ -0,0 +1,51 @@
+import 'package:e_commerce/Core/utils/styles/app_colors.dart';
+import 'package:e_commerce/features/Auth/Login/Presentation/views/widgets/CustomAppBar.dart';
+import 'package:flutter/material.dart';
+import '../../../../../../Core/utils/styles/textStyles.dart';
+import 'CustomTextField.dart';
+
+class LoginViewBody extends StatefulWidget {
+  const LoginViewBody({super.key});
+
+  @override
+  State<LoginViewBody> createState() => _LoginViewBodyState();
+}
+
+class _LoginViewBodyState extends State<LoginViewBody> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool isPasswordObscure = true;
+
+  void updateState() {
+    setState(() {
+      print(isPasswordObscure);
+      isPasswordObscure = !isPasswordObscure;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+        child: Column(
+          children: [
+            CustomAppBar(title: "تسجيل الدخول", onPress: () {}),
+            const SizedBox(
+              height: 24,
+            ),
+            CustomTextField(
+              isPasswordObscure:isPasswordObscure,
+              emailController: emailController,
+              passwordController: passwordController,
+              onPressed: updateState,
+            ),
+            const SizedBox(height: 33,),
+            // ElevatedButton(onPressed: (){}, child: child)
+            Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
