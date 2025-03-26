9baf27117b97500378e80481eda436e2676048a3
osama <osamamoahmed242@gmail.com>
2025-03-26 14:25:03 +0200

    feat: replace CheckOut widget with CheckOutView for improved structure and clarity

---
+++ lib/features/CheckOut/presentation/view/CheckOutView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/CheckOutBody.dart';
+
+class CheckOutView extends StatelessWidget {
+  const CheckOutView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CheckOutViewBody(),
+    );
+  }
+}
